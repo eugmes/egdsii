cfea6cb84b405166e83312be98fd2ce4b26b81f3
Eugeniy Meshcheryakov <eugen@debian.org>
2010-11-16 14:53:25 +0100

    Add arguments handling

--- src/txt2gds.erl
+++ src/txt2gds.erl
@@ -1,104 +1,146 @@
 %% Author: Eugeniy Meshcheryakov <eugen@debian.org>
 %% This file is in the public domain.
 -module(txt2gds).
--export([start/0]).
+-export([start/0, run/1, run/2]).
 
 -include("gdsii.hrl").
 
 -record(state, {
-  last_token = none :: tuple() | 'none',
+  last_token = none :: tuple() | 'none' | 'eof',
   line = 1 :: pos_integer()
 }).
+
+-type io() :: file:io_device() | 'standard_io'.
 
 -spec start() -> no_return().
 
 start() ->
-  run("test.gds"), % FIXME
-  init:stop().
+  case init:get_plain_arguments() of
+    ["-o", OutFileName | Rest] ->
+      case Rest of
+        [InFileName] when InFileName /= "-", hd(InFileName) /= $- ->
+          run(OutFileName, InFileName);
+        ["-"] ->
+          run(OutFileName);
+        [] ->
+          run(OutFileName);
+        _ ->
+          usage()
+      end,
+      init:stop();
+    _ ->
+      usage()
+  end.
+
+-spec usage() -> no_return().
+
+usage() ->
+  io:put_chars(standard_error, <<"Usage: txt2gds -o file.gds [file.txt]\n">>),
+  init:stop(1).
 
 -spec run(string()) -> 'ok'.
 
-run(FileName) ->
-  case file:open(FileName, [write, binary, raw]) of
+run(OutFileName) ->
+  case file:open(OutFileName, [write, binary, raw]) of
     {ok, Dev} ->
-      write_gds(Dev),
+      write_gds(Dev, standard_io),
       file:close(Dev)
   end.
 
--spec write_gds(file:io_device()) -> 'ok'.
+-spec run(string(), string()) -> 'ok'.
 
-write_gds(Dev) ->
-  write_gds(Dev, #state{}).
+run(OutFileName, InFileName) ->
+  case file:open(OutFileName, [write, binary, raw]) of
+    {ok, Dev} ->
+      case file:open(InFileName, [read, read_ahead]) of
+        {ok, Input} ->
+          write_gds(Dev, Input),
+          file:close(Input),
+          file:close(Dev)
+      end
+  end.
 
--spec write_gds(file:io_device(), #state{}) -> 'ok'.
+-spec write_gds(file:io_device(), io()) -> 'ok'.
 
-write_gds(Dev, State) ->
-  case read_record(Dev, State) of
-    {ok, NewState} -> write_gds(Dev, NewState);
+write_gds(Dev, Input) ->
+  write_gds(Dev, Input, #state{}).
+
+-spec write_gds(file:io_device(), io(), #state{}) -> 'ok'.
+
+write_gds(_Dev, _Input, #state{last_token = eof}) ->
+  ok;
+
+write_gds(Dev, Input, State) ->
+  case read_record(Dev, Input, State) of
+    {ok, NewState} -> write_gds(Dev, Input, NewState);
     eof -> ok
   end.
 
-get_token(Line) ->
-  io:request(standard_io, {get_until, "", gdsdump_lex, token, [Line]}).
+-spec get_token(io(), pos_integer()) -> any().
 
--spec read_record(file:io_device(), #state{}) -> {'ok', #state{}} | eof.
+get_token(Input, Line) ->
+  io:request(Input, {get_until, "", gdsdump_lex, token, [Line]}).
 
-read_record(Dev, #state{last_token = {tag, _L, Name}, line = Line}) ->
-  read_record_data(Dev, Name, Line);
+-spec read_record(file:io_device(), io(), #state{}) -> {'ok', #state{}} | eof.
 
-read_record(Dev, #state{last_token = none, line = Line}) ->
-  case get_token(Line) of
+read_record(Dev, Input, #state{last_token = {tag, _L, Name}, line = Line}) ->
+  read_record_data(Dev, Input, Name, Line);
+
+read_record(Dev, Input, #state{last_token = none, line = Line}) ->
+  case get_token(Input, Line) of
     {ok, {tag, _L, Name}, NewLine} ->
-      read_record_data(Dev, Name, NewLine);
+      read_record_data(Dev, Input, Name, NewLine);
     {eof, _} -> eof
   end.
 
--spec read_record_data(file:io_device(), binary(), pos_integer()) -> {'ok', #state{}}.
+-spec read_record_data(file:io_device(), io(), binary(), pos_integer()) -> {'ok', #state{}}.
 
-read_record_data(Dev, Name, Line) ->
+read_record_data(Dev, Input, Name, Line) ->
   Tag = gdsii:name_to_tag(Name),
   {Data, NewLine, Cont} = case gdsii:type_of_tag(Tag) of
     nodata ->
       {nodata, Line, none};
     D ->
-      case get_token(Line) of
+      case get_token(Input, Line) of
         {ok, {colon, _}, NewLine1} ->
-          read_data(D, NewLine1)
+          read_data(Input, D, NewLine1)
       end
   end,
   BinData = gdsii:record_to_binary({Tag, Data}),
   file:write(Dev, BinData),
   {ok, #state{last_token = Cont, line = NewLine}}.
 
-read_data(bitarray, Line) ->
-  case get_token(Line) of
+read_data(Input, bitarray, Line) ->
+  case get_token(Input, Line) of
     {ok, {integer, _L, I}, NewLine} ->
       {{bitarray, I}, NewLine, none}
   end;
 
-read_data(IntType, Line) when IntType =:= int2; IntType =:= int4 ->
-  {List, NewLine, Cont} = read_list(integer, Line),
+read_data(Input, IntType, Line) when IntType =:= int2; IntType =:= int4 ->
+  {List, NewLine, Cont} = read_list(Input, integer, Line),
   {{IntType, List}, NewLine, Cont};
 
-read_data(real8, Line) ->
-  {List, NewLine, Cont} = read_list(float, Line),
+read_data(Input, real8, Line) ->
+  {List, NewLine, Cont} = read_list(Input, float, Line),
   {{real8, List}, NewLine, Cont};
 
-read_data(ascii, Line) ->
-  case get_token(Line) of
+read_data(Input, ascii, Line) ->
+  case get_token(Input, Line) of
     {ok, {string, _L, S}, NewLine} ->
       {{ascii, S}, NewLine, none}
   end.
 
-read_list(Type, Line) ->
-  case get_token(Line) of
+-spec read_list(io(), atom(), pos_integer()) -> {list(), pos_integer(), tuple() | 'eof'}.
+
+read_list(Input, Type, Line) ->
+  case get_token(Input, Line) of
     {ok, {Type, _, Val}, NewLine} ->
-      case get_token(NewLine) of
+      case get_token(Input, NewLine) of
         {ok, {comma, _}, NewLine1} ->
-          {RetList, RetLine, RetCont} = read_list(Type, NewLine1),
+          {RetList, RetLine, RetCont} = read_list(Input, Type, NewLine1),
           {[Val | RetList], RetLine, RetCont};
         {ok, Cont, NewLine1} ->
           {[Val], NewLine1, Cont};
-        {eof, _} -> {[Val], NewLine, none}
+        {eof, _} -> {[Val], NewLine, eof}
       end
   end.
